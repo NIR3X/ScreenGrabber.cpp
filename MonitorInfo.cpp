fd167c0d674897d5d5ef81bbe97dcf3664ea6b22
NIR3X <90041219+NIR3X@users.noreply.github.com>
2024-02-18 22:02:37 +0000

    Update MonitorInfo.cpp

--- MonitorInfo.cpp
+++ MonitorInfo.cpp
@@ -7,7 +7,7 @@ std::vector<SMonitorInfo> GetAllMonitorsInfo() {
 	std::thread([&monitors] {
 		static HMODULE hUser32 = LoadLibraryA("user32.dll");
 		static auto pSetThreadDpiAwarenessContext = (PVOID(WINAPI*)(PVOID))GetProcAddress(hUser32, "SetThreadDpiAwarenessContext");
-		pSetThreadDpiAwarenessContext((PVOID)-3/* DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE */);
+		pSetThreadDpiAwarenessContext((PVOID)-3 /* DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE */);
 
 		EnumDisplayMonitors(nullptr, nullptr, [] (HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) -> BOOL {
 			((std::vector<SMonitorInfo>*)dwData)->push_back({
