8ba8947679eb5e7cbe0e1fc00bacb6f713376688
NIR3X <90041219+NIR3X@users.noreply.github.com>
2024-02-18 21:39:17 +0000

    Update MonitorInfo.cpp

--- MonitorInfo.cpp
+++ MonitorInfo.cpp
@@ -1,29 +1,26 @@
 #include "MonitorInfo.h"
-
-typedef enum PROCESS_DPI_AWARENESS {
-	PROCESS_DPI_UNAWARE = 0,
-	PROCESS_SYSTEM_DPI_AWARE = 1,
-	PROCESS_PER_MONITOR_DPI_AWARE = 2
-} PROCESS_DPI_AWARENESS;
-
-STDAPI SetProcessDpiAwareness(PROCESS_DPI_AWARENESS value);
+#include <thread>
 
 std::vector<SMonitorInfo> GetAllMonitorsInfo() {
-	SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE);
-
 	std::vector<SMonitorInfo> monitors = {};
 
-	EnumDisplayMonitors(nullptr, nullptr, [] (HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) -> BOOL {
-		((std::vector<SMonitorInfo>*)dwData)->push_back({
-			.hMonitor = hMonitor,
-			.hdcMonitor = hdcMonitor,
-			.left = lprcMonitor->left,
-			.top = lprcMonitor->top,
-			.width = lprcMonitor->right - lprcMonitor->left,
-			.height = lprcMonitor->bottom - lprcMonitor->top
-		});
-		return TRUE;
-	}, (LPARAM)&monitors);
+	std::thread([&monitors] {
+		static HMODULE hUser32 = LoadLibraryA("user32.dll");
+		static auto pSetThreadDpiAwarenessContext = (void*(WINAPI*)(void*))GetProcAddress(hUser32, "SetThreadDpiAwarenessContext");
+		pSetThreadDpiAwarenessContext((void*)-3/* DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE */);
+
+		EnumDisplayMonitors(nullptr, nullptr, [] (HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) -> BOOL {
+			((std::vector<SMonitorInfo>*)dwData)->push_back({
+				.hMonitor = hMonitor,
+				.hdcMonitor = hdcMonitor,
+				.left = lprcMonitor->left,
+				.top = lprcMonitor->top,
+				.width = lprcMonitor->right - lprcMonitor->left,
+				.height = lprcMonitor->bottom - lprcMonitor->top
+			});
+			return TRUE;
+		}, (LPARAM)&monitors);
+	}).join();
 
 	return monitors;
 }
